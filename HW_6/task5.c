3102ea64010c3173fe383b9737beaf856f266e05
Gleb <Gleb23112006@mail.ru>
2023-10-22 21:04:32 +0300

    modificate HW_6

--- HW_6/task5.c
+++ HW_6/task5.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
 int main(){
-	
+	int n;
+	printf("put number: ");
+	scanf("%d",&n);
+	unsigned long int sum = 0;
+	for(int i = 0; i < n; i++){
+		sum += (n + i)*(n + i);
+	}
+	printf("equal of square of whole numbers in (n ... 2n) = %lu\n",sum);
 	return 0;
 }
